264c1216bcd15ade1f2e55bbfc42a3cf4a34bb1a
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-09 17:42:16 +0530

    new_collection.js added

---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,49 @@
+import p12 from './ProductImages/Product12.jpg'
+import p13 from './ProductImages/Product13.jpg'
+import p17 from './ProductImages/Product17.jpg'
+import p20 from './ProductImages/Product20.jpg'
+
+let new_collections = [
+    {
+        id: 17,
+        name: "LEATHER EFFECT CROPPED BIKER",
+        category: "women",
+        sub_category: "casual",
+        image: p17 ,
+        new_price: "2950",
+        old_price: "3400",
+        brand:"Zara"
+    },
+    {
+        id: 12,
+        name: "COLOUR BLOCK POLO T-SHIRT WITH SLOGANS",
+        category: "mens",
+        sub_category: "party",
+        image: p12,
+        new_price: "2950",
+        old_price: "3500",
+        brand:"Zara"
+    },
+    {
+        id: 20,
+        name: "RIBBED LONG SLEEVE T-SHIRT",
+        category: "women",
+        sub_category: "gym",
+        image: p20,
+        new_price: "1550",
+        old_price: "1800",
+        brand: "Zara"
+    },
+    {
+        id: 13,
+        name: "TIE-DYE FLOWING SHIRT",
+        category: "mens",
+        sub_category: "casual",
+        image: p13 ,
+        new_price: "4350",
+        old_price: "4800",
+        brand:"Zara"
+    }
+]
+
+export default new_collections;
