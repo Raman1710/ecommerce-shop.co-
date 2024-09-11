10492fa311bb9f7a5323d2b6ec06c33a83bd68ad
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-11 09:59:30 +0530

    Items.jsx updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -6,14 +6,14 @@ function Items(props) {
   const discountPercentage = ((oldPrice - newPrice) / oldPrice) * 100;
 
   return (
-    <div className='flex flex-col items-start  hover:bg-[#F0EEED] hover:border-2 hover:scale-105 rounded-3xl p-3 w-[310px]'>
+    <div className='flex flex-col items-start hover:bg-black hover:text-white hover:border-2 hover:scale-105 rounded-3xl p-3 w-[310px]'>
         <img src={props.image} className='rounded-xl flex justify-center w-full h-full'/>
         <p className=' font-satoshi-bold font-bold text-[24px] mt-4 mb-2 text-center w-full leading-tight h-[60px]'>{props.name}</p>
         <div className='flex flex-row gap-3 justify-center w-full'>
-            <div className=' font-satoshi-bold font-bold text-[24px]'>
+            <div className=' font-satoshi-bold font-bold text-[24px] !hover:text-white'>
             ₹{newPrice.toFixed(0)}
             </div>
-            <div className='font-satoshi-bold font-bold text-[20px] line-through text-black/40 flex items-center'>
+            <div className='font-satoshi-bold font-bold text-[20px] line-through    flex items-center'>
             ₹{oldPrice.toFixed(0)}
             </div>
             <div className='bg-[#ff3333]/10 text-[#ff3333]/100 flex justify-center items-center  rounded-[62px] text-[12px] px-[13.5px] py-[6px] font-satoshi-regular font-medium'>
