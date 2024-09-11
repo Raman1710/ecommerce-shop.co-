f48411cd6255383a32ef7c6a9baeb2a9fbcaf503
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-11 22:38:03 +0530

    PrimaryButton.jsx updated

--- src/Components/Buttons/PrimaryButton.jsx
+++ src/Components/Buttons/PrimaryButton.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function PrimaryButton({children, className, onClick,}) {
 
   return (
-    <div>
-    <button className={`mt-[37px] bg-black text-white px-[67.5px] py-[15px] rounded-[62px] hover:underline ${className}`} onClick={onClick}>{children}</button>
-    </div>
+    
+    <button className={`mt-[37px] bg-black px-[67.5px] py-[15px] rounded-[62px] hover:underline ${className}`} onClick={onClick}>{children}</button>
+    
   )
 }
 
