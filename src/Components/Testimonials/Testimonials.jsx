02a995360c29a756499b1abd3f31d5d6182e124e
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-11 22:40:25 +0530

    Testimonials.jsx updated

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import Carousel from './Carousel'
 
 function Testimonials() {
   return (
-    <div className=' mt-20 '>
+    <div className=' mt-20 pb-[80px] '>
         <div className='mx-[148px]'>
             <p className=' font-integral font-black text-[48px] tracking-tighter'>OUR HAPPY CUSTOMERS</p>
         </div>
@@ -12,7 +12,7 @@ function Testimonials() {
         <Carousel name='Sarah M.' message= 'I am blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I have bought has exceeded my expectations.' />
         <Carousel name='Alex K.' message= 'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.' />
         <Carousel name='James L.' message= "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends." />
-        <Carousel name='Sarah M.' message= 'I am blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I have bought has exceeded my expectations.' />
+        {/* <Carousel name='Sarah M.' message= 'I am blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I have bought has exceeded my expectations.' /> */}
         </div>
 
     </div>
