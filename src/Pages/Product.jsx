18f67bd040e89c1bf3ef1fb2e47a07238cb3cc74
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-27 22:24:12 +0530

    Product.jsx updated

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   
@@ -10,8 +11,9 @@ const Product = () => {
   const product = all_product.find((e) => e.id === Number(productId));
 
   return (
-    <div>
-      <Breadcrumb product = {product} />
+    <div className='mx-[148px] border-t-2 '>
+      <Breadcrumb product = {product}  />
+      <ProductDisplay product= {product}/>
     </div>
   )
 }
