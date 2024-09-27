d5e33aa3158b13a954d5ff3ee62f6317dc0dae08
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-27 23:09:50 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -9,7 +9,7 @@ function Items(props) {
   return (
     <div className='flex flex-col items-start hover:bg-black hover:text-white hover:border-2 hover:scale-105 rounded-3xl p-3 w-[310px] '>
        <Link to={`/product/${props.id}`}>
-            <img src={props.image} className='rounded-xl flex justify-center w-full h-[429.5px]'/>
+            <img src={props.image} className='rounded-xl flex justify-center w-full h-[429.5px] ' onClick={window.scrollTo(0,0)}/>
        </Link> 
         <p className=' font-satoshi-bold font-bold text-[24px] mt-4 mb-2 text-center w-full leading-tight h-[60px] text-clip capitalize'>{props.name}</p>
         <div className='flex flex-row gap-3 justify-center w-full'>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,8 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
   
@@ -14,6 +16,8 @@ const Product = () => {
     <div className='mx-[148px] border-t-2 '>
       <Breadcrumb product = {product}  />
       <ProductDisplay product= {product}/>
+      <DescriptionBox/>
+      <RelatedProducts/>
     </div>
   )
 }
