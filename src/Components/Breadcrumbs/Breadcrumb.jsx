e9363930aa55565c453115a9075eb79f66004fbb
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-09-27 22:23:20 +0530

    Breadcrumb.jsx Updated

--- src/Components/Breadcrumbs/Breadcrumb.jsx
+++ src/Components/Breadcrumbs/Breadcrumb.jsx
@@ -3,8 +3,8 @@ import arrowIcon from '../Assets/arrow_icon.svg'
 const Breadcrumb = (props) => {
     const {product} = props;
   return (
-    <div className='flex flex-row'>
-        HOME <img src={arrowIcon}/> SHOP <img src={arrowIcon}/> {product.category} <img src={arrowIcon}/> {product.sub_category} <img src={arrowIcon}/> {product.name}
+    <div className='flex flex-row mt-6 mb-9 font-satoshi-regular '>
+        HOME <img src={arrowIcon}/> SHOP <img src={arrowIcon}/> {product.category.toUpperCase()} <img src={arrowIcon}/> {product.sub_category.toUpperCase()} <img src={arrowIcon}/> {product.name.toUpperCase()}
     </div>
   )
 }
