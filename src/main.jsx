6a75c4b1813fbd03dcec712597d0f3866cd80651
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-10-03 15:22:49 +0530

    react-toastify added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ShopContextProvider from './Context/ShopContext.jsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
   <ShopContextProvider>
     <App />
+    <ToastContainer/>
   </ShopContextProvider>
-  // </StrictMode>,
+   </StrictMode>
 )
